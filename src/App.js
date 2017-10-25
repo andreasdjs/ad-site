d1de2e21672214b00beb342ba003c521f0958e08
Andreas Dagnell <andreasdagnell@Andreas-Jobbmaskin.local>
2017-10-25 14:57:43 +0200

    Added SCSS variables & CSS reset.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+import thelogo from './images/thelogo.png';
+import background from './images/bg.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div><img src={background} className="background-image" alt="thebackground" /></div>
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Hello world</h1>
+          <img src={thelogo} className="App-logo" alt="thelogo" />
+          <img src={background} className="background-image" alt="thebackground" />
+          <h1 className="App-title">TITLE TEXT</h1>
         </header>
         {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
