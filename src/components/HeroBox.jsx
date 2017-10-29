035b061c1aa93a9665d94d005c39e939e4f41da8
andreasdjs <andreas.dagnell@gmail.com>
2017-10-29 20:23:15 +0100

    Updated copy and sizes.

--- src/components/HeroBox.jsx
+++ src/components/HeroBox.jsx
@@ -5,7 +5,9 @@ class HeroBox extends React.Component {
     return (
       <div className="firstpage-hero">
         <div className="title"><h1 className="tk-museo-sans">ANDREAS DAGNELL</h1></div>
-        <div className="subtitle"><h3 className="tk-museo-sans"> <i>Gesamtkunztwerk</i></h3></div>
+        <div className="subtitle"><h3 className="tk-museo-sans">CREATOR OF DIGITAL GESAMTKUNSTWERK</h3></div>
+        {/* <div className="subtitle"><h3 className="tk-museo-sans"> <i>DIGITAL GESAMTKUNSTWERK</i></h3></div> */}
+        {/* <div className="subtitle"><h3 className="tk-museo-sans"> <i>digital gesamtkunstwerk</i></h3></div> */}
       </div>
     );
   }
