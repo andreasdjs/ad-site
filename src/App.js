1395cefa2b443fa11d030e0c463db26514b4c96a
Andreas Dagnell <andreasdagnell@Andreas-Jobbmaskin.local>
2017-10-30 15:06:21 +0100

    Added breakpoints for grid and headers.

--- src/App.js
+++ src/App.js
@@ -16,15 +16,10 @@ class App extends Component {
         <HeroBox />
         <GridBox />
         <FooterBox />
-        <div><img src={background} className="background-image" alt="thebackground" /></div>
-        {/* <header className="App-header">
-          <img src={thelogo} className="App-logo" alt="thelogo" />
-          <img src={anotherlogo} className="App-logo" alt="thelogo" />
-          <img src={background} className="background-image" alt="thebackground" />
-        </header> */}
-        {/* <div className="firstpage-hero">
-          <h1 className="App-title tk-museo-sans">MUSEO SANS title text ABCDEFGH abcdefgh</h1>
-        </div> */}
+        {/* <div><img src={background} className="background-image" alt="thebackground" /></div> */}
+        {/* <img src={thelogo} className="App-logo" alt="thelogo" />
+        <img src={anotherlogo} className="App-logo" alt="thelogo" />
+        <img src={background} className="background-image" alt="thebackground" /> */}
       </div>
     );
   }
